79fd5565bdf3777923bd6e7ed71ab1f4109d5a0c
ovasconceloss <victorolimpiovasconcelos.dev@gmail.com>
2025-09-16 19:51:01 +0100

    feat: create a route to the home page

--- frontend/src/routes/appRouter.tsx
+++ frontend/src/routes/appRouter.tsx
@@ -1,10 +1,12 @@
-import { BrowserRouter as Router, Routes } from "react-router-dom"
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom"
+
+import Home from "../pages/Home";
 
 const App: React.FC = () => {
   return (
     <Router>
       <Routes>
-
+        <Route path="/" element={ <Home /> } />
       </Routes>
     </Router>
   )
