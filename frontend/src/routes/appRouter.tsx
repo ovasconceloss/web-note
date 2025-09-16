ca86b55694ef5961e6116f818a8967a45bc31454
ovasconceloss <victorolimpiovasconcelos.dev@gmail.com>
2025-09-16 18:50:29 +0100

    feat: create the client's main route grouper

---
+++ frontend/src/routes/appRouter.tsx
@@ -0,0 +1,13 @@
+import { BrowserRouter as Router, Routes } from "react-router-dom"
+
+const App: React.FC = () => {
+  return (
+    <Router>
+      <Routes>
+
+      </Routes>
+    </Router>
+  )
+}
+
+export default App;
