eaaadfcb7e76100fa376d69534e85a6569ac041a
ovasconceloss <victorolimpiovasconcelos.dev@gmail.com>
2025-09-17 11:16:44 +0100

    feat: create a route to the registration page

--- frontend/src/routes/appRouter.tsx
+++ frontend/src/routes/appRouter.tsx
@@ -1,12 +1,14 @@
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom"
 
 import Home from "../pages/Home";
+import SignUp from "../pages/SignUp";
 
 const App: React.FC = () => {
   return (
     <Router>
       <Routes>
         <Route path="/" element={ <Home /> } />
+        <Route path="/signup" element={ <SignUp /> } />
       </Routes>
     </Router>
   )
