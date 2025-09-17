5e183f4e18a02c6ded1e753a5c25ec8fb2f2eb74
ovasconceloss <victorolimpiovasconcelos.dev@gmail.com>
2025-09-17 10:46:33 +0100

    feat: add a scale style to the document

--- frontend/src/components/Document.tsx
+++ frontend/src/components/Document.tsx
@@ -1,6 +1,6 @@
 export const Document: React.FC = () => {
   return (
-    <article className="w-[20rem] p-2 space-y-2 rounded-sm">
+    <article className="w-[20rem] p-2 space-y-2 rounded-sm cursor-pointer transition-all ease-in-out hover:scale-105">
       <div className="flex items-center justify-between">
         <h3 className="text-2xl">New Document</h3>
         <div>
