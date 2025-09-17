4b575d4b197ecba21d37ab400012bb30c3739c54
ovasconceloss <victorolimpiovasconcelos.dev@gmail.com>
2025-09-17 11:19:15 +0100

    feat: add functional links to the header
    - link to the login page
    - link to the registration page
    - link to the home page again

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -4,7 +4,7 @@ export const Header: React.FC = () => {
   return (
     <header className="w-full p-5 flex items-center justify-between select-none">
       <section>
-        <h1 className="text-2xl font-bold">WebNote</h1>
+        <Link to={'/'} className="text-2xl font-bold cursor-pointer transition-all ease-in-out hover:text-zinc-500">WebNote</Link>
       </section>
       <section className="space-x-5">
         <Link to={'/'}>
@@ -13,7 +13,7 @@ export const Header: React.FC = () => {
               w-[6.0rem] p-1 rounded-sm bg-transparent border-2 border-zinc-900 text-zinc-900 cursor-pointer
               transition-all ease-in-out hover:bg-zinc-900 hover:text-zinc-100">Sign In</button>
         </Link>
-        <Link to={'/'} >
+        <Link to={'/signup'} >
           <button 
             className="
               w-[6.0rem] p-1 rounded-sm bg-zinc-900 border-2 border-zinc-900 text-zinc-100 cursor-pointer
