01fa189bbe68dd6935d6e199c9174e5ca38c6365
ovasconceloss <victorolimpiovasconcelos.dev@gmail.com>
2025-09-17 10:39:49 +0100

    feat: change the styling of the ‘document’ elements
    - change text sizes
    - change spacing (y) between elements

--- frontend/src/components/Document.tsx
+++ frontend/src/components/Document.tsx
@@ -1,17 +1,17 @@
 export const Document: React.FC = () => {
   return (
-    <article className="w-[15rem] p-2 space-y-1 rounded-sm">
+    <article className="w-[20rem] p-2 space-y-2 rounded-sm">
       <div className="flex items-center justify-between">
-        <h3 className="text-lg">New Document</h3>
+        <h3 className="text-2xl">New Document</h3>
         <div>
-          <h4 className="text-lg">Public</h4>
+          <h4 className="text-xl">Public</h4>
         </div>
       </div>
       <div>
-        <h4 className="text-md">Created by Jonh Doe</h4>
+        <h4 className="text-lg">Created by Jonh Doe</h4>
       </div>
       <div>
-        <h4 className="text-md">Last edited 20 days ago</h4> 
+        <h4 className="text-lg">Last edited 20 days ago</h4> 
       </div>
     </article>
   );
