f8eead04f20979750551dedab90eb906d9b4575d
ovasconceloss <victorolimpiovasconcelos.dev@gmail.com>
2025-09-17 10:14:42 +0100

    feat: create header elements
    - create title (application name)
    - create buttons for account (login and registration)

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,7 +1,25 @@
+import { Link } from "react-router-dom";
+
 export const Header: React.FC = () => {
   return (
-    <header>
-
+    <header className="w-full p-5 flex items-center justify-between select-none">
+      <section>
+        <h1 className="text-2xl font-bold">WebNote</h1>
+      </section>
+      <section className="space-x-5">
+        <Link to={'/'}>
+          <button 
+            className="
+              w-[6.0rem] p-1 rounded-sm bg-transparent border-2 border-zinc-900 text-zinc-900 cursor-pointer
+              transition-all ease-in-out hover:bg-zinc-900 hover:text-zinc-100">Sign In</button>
+        </Link>
+        <Link to={'/'} >
+          <button 
+            className="
+              w-[6.0rem] p-1 rounded-sm bg-zinc-900 border-2 border-zinc-900 text-zinc-100 cursor-pointer
+              transition-all ease-in-out hover:bg-zinc-100 hover:text-zinc-900">Sign Up</button>
+        </Link>
+      </section>
     </header>
   );
 };
