8b54f45eb1caeae6ab5d89fa83030cd0f7be9acc
ovasconceloss <victorolimpiovasconcelos.dev@gmail.com>
2025-09-17 11:43:14 +0100

    feat: add style to the registration form elements

--- frontend/src/pages/SignUp.tsx
+++ frontend/src/pages/SignUp.tsx
@@ -3,6 +3,7 @@ import { useForm, type SubmitHandler } from "react-hook-form"
 
 type FormInput = {
   email: string,
+  confirm: string,
   password: string,
   username: string,
 }
@@ -14,33 +15,51 @@ const SignUp: React.FC = () => {
   return (
     <>
     <Header/>
-    <main className="p-5 space-y-10 select-none">
-      <form onSubmit={handleSubmit(onSubmit)} className="space-y-5">
+    <main className="flex flex-col items-center p-5 space-y-15 select-none">
+      <section>
+        <h1 className="text-2xl font-bold">Welcome! Sign up now!</h1>
+      </section>
+      <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col items-center space-y-8">
         <div className="flex flex-col space-y-2">
-          <label htmlFor="username">Username</label>
+          <label htmlFor="username" className="text-lg">Username</label>
           <input 
-            autoComplete={"false"}
+            autoComplete="off"
             {...register("username", { required: true })}
-            className="w-[20rem] p-1 border-1 border-zinc-300 rounded-sm outline-none"
+            className="w-[25rem] p-1 border-1 border-zinc-300 rounded-sm outline-none text-lg
+            transition-all ease-in-out focus:border-zinc-900"
           />
         </div>
         <div className="flex flex-col space-y-2">
-          <label htmlFor="email">Email Address</label>
+          <label htmlFor="email" className="text-lg">Email Address</label>
           <input 
-            autoComplete={"false"}
+            autoComplete="off"
             {...register("email", { required: true })}
-            className="w-[20rem] p-1 border-1 border-zinc-300 rounded-sm outline-none"
+            className="w-[25rem] p-1 border-1 border-zinc-300 rounded-sm outline-none text-lg
+            transition-all ease-in-out focus:border-zinc-900"
           />
         </div>
         <div className="flex flex-col space-y-2">
-          <label htmlFor="password">Password</label>
+          <label htmlFor="password" className="text-lg">Password</label>
           <input 
-            autoComplete={"false"}
+            autoComplete="off"
             {...register("password", { required: true })}
-            className="w-[20rem] p-1 border-1 border-zinc-300 rounded-sm outline-none"
+            className="w-[25rem] p-1 border-1 border-zinc-300 rounded-sm outline-none text-lg
+            transition-all ease-in-out focus:border-zinc-900"
           />
         </div>
-        <button type="submit">Sign Up</button>
+        <div className="flex flex-col space-y-2">
+          <label htmlFor="confirm" className="text-lg">Confirm Password</label>
+          <input 
+            autoComplete="off"
+            {...register("confirm", { required: true })}
+            className="w-[25rem] p-1 border-1 border-zinc-300 rounded-sm outline-none text-lg
+            transition-all ease-in-out focus:border-zinc-900"
+          />
+        </div>
+        <button 
+          type="submit"
+          className="w-[25rem] p-2 text-lg bg-zinc-900 border-2 border-zinc-900 text-zinc-100 rounded-sm cursor-pointer
+          transition-all ease-in-out hover:bg-transparent hover:text-zinc-900">Create account</button>
       </form>
     </main>
     </>
