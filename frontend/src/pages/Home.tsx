6f8c40e60dcff6a86fada086e9d722e331e07d15
ovasconceloss <victorolimpiovasconcelos.dev@gmail.com>
2025-09-17 10:32:17 +0100

    feat: create elements for the main part of the home page
    - create a section to display all user documents

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -4,6 +4,14 @@ const Home: React.FC = () => {
   return (
     <>
     <Header />
+    <main className="p-5 space-y-10 bg-zinc-100">
+      <section className="space-y-1">
+        <h2 className="text-3xl font-bold">Your Documents</h2>
+        <p className="text-lg">Manage and organize your documents with ease</p>
+      </section>
+      <section>
+      </section>
+    </main>
     </>
   );
 };
