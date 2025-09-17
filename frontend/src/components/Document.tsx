600fd750eabc2b199cb465931c5ac1fd4ef7de91
ovasconceloss <victorolimpiovasconcelos.dev@gmail.com>
2025-09-17 10:51:22 +0100

    chore: change document styles
    - style public and private badges
    - style borders (bottom)

--- frontend/src/components/Document.tsx
+++ frontend/src/components/Document.tsx
@@ -1,10 +1,12 @@
 export const Document: React.FC = () => {
   return (
-    <article className="w-[20rem] p-2 space-y-2 rounded-sm cursor-pointer transition-all ease-in-out hover:scale-105">
+    <article 
+      className="w-[20rem] p-2 space-y-2 rounded-sm cursor-pointer border-b-2 rounded-b-none 
+      border-zinc-300 transition-all ease-in-out hover:scale-105 hover:border-zinc-900">
       <div className="flex items-center justify-between">
         <h3 className="text-2xl">New Document</h3>
         <div>
-          <h4 className="text-xl">Public</h4>
+          <h4 className="w-[5.0rem] text-lg bg-zinc-900 rounded-full text-zinc-100 text-center">Public</h4>
         </div>
       </div>
       <div>
