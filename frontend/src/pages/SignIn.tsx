2327244ec0e21973030d0094788db42176c2eb78
ovasconceloss <victorolimpiovasconcelos.dev@gmail.com>
2025-09-19 14:36:25 +0100

    feat: create the login page and its elements

---
+++ frontend/src/pages/SignIn.tsx
@@ -0,0 +1,49 @@
+import { Header } from "../components/Header";
+import { useForm, type SubmitHandler } from "react-hook-form"
+
+type FormInput = {
+  email: string,
+  password: string,
+}
+
+const SignIn: React.FC = () => {
+  const { register, handleSubmit } = useForm<FormInput>();
+  const onSubmit: SubmitHandler<FormInput> = (data) => console.log(data);
+
+  return (
+    <>
+    <Header/>
+    <main className="flex flex-col items-center p-5 space-y-15 select-none">
+      <section>
+        <h1 className="text-2xl font-bold">Welcome Back! Sign in now!</h1>
+      </section>
+      <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col items-center space-y-8">
+        <div className="flex flex-col space-y-2">
+          <label htmlFor="email" className="text-lg">Email Address</label>
+          <input 
+            autoComplete="off"
+            {...register("email", { required: true })}
+            className="w-[25rem] p-1 border-1 border-zinc-300 rounded-sm outline-none text-lg
+            transition-all ease-in-out focus:border-zinc-900"
+          />
+        </div>
+        <div className="flex flex-col space-y-2">
+          <label htmlFor="password" className="text-lg">Password</label>
+          <input 
+            autoComplete="off"
+            {...register("password", { required: true })}
+            className="w-[25rem] p-1 border-1 border-zinc-300 rounded-sm outline-none text-lg
+            transition-all ease-in-out focus:border-zinc-900"
+          />
+        </div>
+        <button 
+          type="submit"
+          className="w-[25rem] p-2 text-lg bg-zinc-900 border-2 border-zinc-900 text-zinc-100 rounded-sm cursor-pointer
+          transition-all ease-in-out hover:bg-transparent hover:text-zinc-900">Enter</button>
+      </form>
+    </main>
+    </>
+  );
+};
+
+export default SignIn;
