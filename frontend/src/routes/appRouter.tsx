f2335b217109e91f1ff1afba3d0a79176946901d
ovasconceloss <victorolimpiovasconcelos.dev@gmail.com>
2025-09-19 14:37:17 +0100

    feat: create a route to the login page

--- frontend/src/routes/appRouter.tsx
+++ frontend/src/routes/appRouter.tsx
@@ -2,6 +2,7 @@ import { Route, BrowserRouter as Router, Routes } from "react-router-dom"
 
 import Home from "../pages/Home";
 import SignUp from "../pages/SignUp";
+import SignIn from "../pages/SignIn";
 
 const App: React.FC = () => {
   return (
@@ -9,6 +10,7 @@ const App: React.FC = () => {
       <Routes>
         <Route path="/" element={ <Home /> } />
         <Route path="/signup" element={ <SignUp /> } />
+        <Route path="/signin" element={ <SignIn /> } />
       </Routes>
     </Router>
   )
